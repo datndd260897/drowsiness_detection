615d45a13b2dc6f7b97a12282f4a01cb61e7ea38
Dat Nguyen <datndd260897@gmail.com>
2026-03-15 20:03:48 -0700

    add enhanced new model

--- main.py
+++ main.py
@@ -10,18 +10,31 @@ from pygame import mixer
 class EyeClassifier(nn.Module):
     def __init__(self):
         super(EyeClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
-        self.fc1 = nn.Linear(64 * 18 * 18, 128)
+        # Input: 1 x 24 x 24
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32) # Stabilizes learning
+        self.pool1 = nn.MaxPool2d(2, 2) # Reduces dimensions to 12x12
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(2, 2) # Reduces dimensions to 6x6
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(2, 2) # Reduces dimensions to 3x3
+
+        # Flattened size: 128 channels * 3 * 3 = 1152
+        self.fc1 = nn.Linear(128 * 3 * 3, 128)
+        self.dropout = nn.Dropout(0.5) # Prevents overfitting
         self.fc2 = nn.Linear(128, 2)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 64 * 18 * 18)
+        x = self.pool1(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool2(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool3(F.relu(self.bn3(self.conv3(x))))
+        x = x.view(-1, 128 * 3 * 3)
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
 
@@ -65,10 +78,10 @@ def main():
     
     model = EyeClassifier().to(device)
     try:
-        model.load_state_dict(torch.load('models/model.pth', map_location=device, weights_only=True))
-        print("Successfully loaded 'models/model.pth'")
+        model.load_state_dict(torch.load('models/best_model_2.pth', map_location=device, weights_only=True))
+        print("Successfully loaded 'models/best_model_2.pth'")
     except FileNotFoundError:
-        print("Error: 'models/model.pth' not found. Please run the training script first.")
+        print("Error: 'models/best_model_2.pth' not found. Please run the training script first.")
         return
 
     model.eval()  # Set model to evaluation mode
